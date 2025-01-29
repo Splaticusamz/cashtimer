2468058d04cd7c5c48275c109741947667b6134d
Sam Zamor <168610299+Splaticusamz@users.noreply.github.com>
2025-01-29 15:07:15 -0800

    fixes for vercel

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -1,10 +1,10 @@
 import { Button, ButtonProps } from '@mantine/core';
 import { ReactNode } from 'react';
 
-interface CustomButtonProps extends Omit<ButtonProps, 'leftIcon'> {
+interface CustomButtonProps extends ButtonProps {
   leftIcon?: ReactNode;
 }
 
 export const CustomButton = ({ leftIcon, ...props }: CustomButtonProps) => (
-  <Button {...props} leftSection={leftIcon} />
+  <Button {...props} leftIcon={leftIcon} />
 ); 
